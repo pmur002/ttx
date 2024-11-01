54ae0ec79231a464ef944aead1ae2841e3c04815
Paul Murrell <paul@stat.auckland.ac.nz>
2024-11-01 14:32:10 +1300

    update test output; set up for vignette

--- tests/ttx.R
+++ tests/ttx.R
@@ -4,19 +4,19 @@ library(ttx)
 font <- system.file("fonts", "Montserrat", "static", "Montserrat-Medium.ttf",
                     package="grDevices")
 
-suppressMessages(ttxListTables(font))
+ttxListTables(font)
 
-suppressMessages(ttxGetTable("head", font))
+ttxGetTable("head", font)
 
-suppressMessages(ttxFontFamily(font))
-suppressMessages(ttxFontWeight(font))
-suppressMessages(ttxFontStyle(font))
+ttxFontFamily(font)
+ttxFontWeight(font)
+ttxFontStyle(font)
        
-suppressMessages(head(ttxGlyphNames(font)))
-suppressMessages(ttxGlyphIndex("A", font))
-suppressMessages(ttxGlyphWidth(1, font))
-suppressMessages(ttxGlyphHeight(1, font)) ## NOTE heights for vertical writing
-suppressMessages(ttxGlyphBounds(1, font))
+head(ttxGlyphNames(font))
+ttxGlyphIndex("A", font)
+ttxGlyphWidth(1, font)
+ttxGlyphHeight(1, font) ## NOTE heights for vertical writing
+ttxGlyphBounds(1, font)
 
 
 
